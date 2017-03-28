99535f79157345f961a89ace74ed82bd5276a85e
Zach <zcleblanc@hotmail.com>
2017-03-28 16:45:51 -0300

    Initital Push

---
+++ Final/Final/APIHandler.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace Final
+{
+    class APIHandler
+    {
+        private String APIResult;
+        private DatabaseHandler DBH = new DatabaseHandler();
+        private HttpClient client = new HttpClient();
+        private Uri gatherer = new Uri("https://api.magicthegathering.io/v1/cards");
+
+
+
+        public void PopulateDatabaseFromApi()
+        {
+            var DB = DBH.DbConnection;
+            //Card card;
+            var temp = client.GetAsync(gatherer);
+
+            //Debug.WriteLine(temp);
+            //List<Card> cards = (from p in DB.Table<Card>() select p).ToList();
+            CallApi();
+
+
+        }
+
+        public async void CallApi()
+        {
+
+            HttpClient webClient = new HttpClient();
+
+            var http = new HttpClient();
+            var response = await http.GetAsync(gatherer);
+            APIResult = await response.Content.ReadAsStringAsync();
+
+            Object temp = JsonConvert.DeserializeObject<Card>(APIResult);
+
+            Debug.WriteLine(temp);
+
+            //Object p2 = (Object)ser.ReadObject(response);
+
+        }
+
+
+
+    }
+}
+
---
+++ Final/Final/Card.cs
@@ -0,0 +1,36 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Final
+{
+    class Card
+    {
+        String name { get; set; }
+        String names { get; set; }
+        String manaCost { get; set;}
+        String cmc { get; set; }
+        String colors { get; set; }
+        String colorIdentity { get; set; }
+        String type { get; set; }
+        String supertypes { get; set; }
+        String types { get; set; }
+        String subtypes { get; set; }
+        String rarity { get; set; }
+        String set { get; set; }
+        String text { get; set; }
+        String artist { get; set; }
+        String number { get; set; }
+        String power { get; set; }
+        String toughness { get; set; }
+        String layout { get; set; }
+        String multiverseid { get; set; }
+        String imageUrl { get; set; }
+
+        
+    }
+}
