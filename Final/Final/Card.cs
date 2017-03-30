a595c39ef1852e21d7fdf75192c0931788cc56b4
Zach <zcleblanc@hotmail.com>
2017-03-29 23:37:01 -0300

    API And Database Complete Fun Fact: The Api only gives 100 results at a time xD

--- Final/Final/Card.cs
+++ Final/Final/Card.cs
@@ -4,33 +4,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
 
 namespace Final
 {
-    class Card
+    public class Legality
     {
-        String name { get; set; }
-        String names { get; set; }
-        String manaCost { get; set;}
-        String cmc { get; set; }
-        String colors { get; set; }
-        String colorIdentity { get; set; }
-        String type { get; set; }
-        String supertypes { get; set; }
-        String types { get; set; }
-        String subtypes { get; set; }
-        String rarity { get; set; }
-        String set { get; set; }
-        String text { get; set; }
-        String artist { get; set; }
-        String number { get; set; }
-        String power { get; set; }
-        String toughness { get; set; }
-        String layout { get; set; }
-        String multiverseid { get; set; }
-        String imageUrl { get; set; }
+        public string format { get; set; }
+        public string legality { get; set; }
+    }
+
+    public class Ruling
+    {
+        public string date { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Card
+    {
+        public string name { get; set; }
+
+        public string manaCost { get; set; }
+        public int cmc { get; set; }
+
+        [TextBlob("colorsBlobbed")]
+        public List<string> colors { get; set; }
+        public string colorsBlobbed { get; set; }
+
+        [TextBlob("colorIdentityBlobbed")]
+        public List<string> colorIdentity { get; set; }
+        public string colorIdentityBlobbed { get; set; }
+
+        public string type { get; set; }
+
+        [TextBlob("typesBlobbed")]
+        public List<string> types { get; set; }
+        public string typesBlobbed { get; set; }
+
+        [TextBlob("subtypesBlobbed")]
+        public List<string> subtypes { get; set; }
+        public string subtypesBlobbed { get; set; }
+
+        public string rarity { get; set; }
+        public string set { get; set; }
+        public string setName { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public string power { get; set; }
+        public string toughness { get; set; }
+        public string layout { get; set; }
+        public int multiverseid { get; set; }
+        public string imageUrl { get; set; }
+
+        [TextBlob("printingsBlobbed")]
+        public List<string> printings { get; set; }
+        public string printingsBlobbed { get; set; }
+
+        public string originalText { get; set; }
+        public string originalType { get; set; }
+
+        [TextBlob("legalitiesBlobbed")]
+        public List<Legality> legalities { get; set; }
+        public string legalitiesBlobbed;
+
+        [PrimaryKey]
+        public string id { get; set; }
+
+        public bool? reserved { get; set; }
+
+        [TextBlob("rulingsBlobbed")]
+        public List<Ruling> rulings { get; set; }
+        public string rulingsBlobbed { get; set; }
+
+        [TextBlob("supertypesBlobbed")]
+        public List<string> supertypes { get; set; }
+        public string supertypesBlobbed { get; set; }
+
+        [TextBlob("variationsBlobbed")]
+        public List<int?> variations { get; set; }
+        public int? variationsBlobbed { get; set; }
 
         
+    }
+
+
+    public class RootObject
+    {
+        public List<Card> cards { get; set; }
     }
 }
---
+++ Final/Final/MainData.cs
@@ -0,0 +1,205 @@
+﻿using Newtonsoft.Json;
+using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Final
+{
+    public class MainData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public LoadBtnCmd LoadBtnCmd { get; }
+        public Card CurrentCard { get; set; }
+        public ObservableCollection<Card> Cards { get; set; }
+        public string CardName { get; set;}
+
+        //private string _filter;
+        private List<Card> _AllCards = new List<Card>();
+        private String APIResult;
+        private HttpClient client = new HttpClient();
+        private Uri gatherer = new Uri("https://api.magicthegathering.io/v1/cards");
+        private Card _selectedCard;
+
+
+
+        public MainData()
+        {
+            LoadBtnCmd = new LoadBtnCmd(this);
+            Cards = new ObservableCollection<Card>();
+
+            DbConnection.CreateTable<Card>();
+            //var info = DBH.DbConnection.GetMapping(typeof(Card));
+
+            LoadCards();
+            //CallApi();
+        }
+
+
+        public Card SelectedCard
+        {
+            get
+            { return _selectedCard; }
+            set
+            {
+                _selectedCard = value;
+                if (value == null)
+                {
+                    CurrentCard = null;
+                }
+                else
+                {
+                    CurrentCard = value;
+                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentCard"));
+            }
+        }
+
+        //public string Filter
+        //{
+        //    get
+        //    {
+        //        return _filter;
+        //    }
+        //    set
+        //    {
+        //        if (value == _filter)
+        //        {
+        //            return;
+        //        }
+        //        _filter = value;
+        //        PropertyChanged?.Invoke(this,
+        //            new PropertyChangedEventArgs(nameof(Filter)));
+        //        PerformFiltering();
+        //    }
+        //}
+
+        //private void PerformFiltering()
+        //{
+        //    if (_filter == null)
+        //    {
+        //        _filter = "";
+        //    }
+        //    var lowerCaseFilter = Filter.ToLowerInvariant().Trim();
+        //    var result = _AllCards.Where(
+        //        d => d.CardsAsString.ToLowerInvariant()
+        //                .Contains(lowerCaseFilter)).ToList();
+        //    var toRemove = Cards.Except(result).ToList();
+        //    foreach (var x in toRemove)
+        //    {
+        //        Cards.Remove(x);
+        //    }
+        //    // Add back in the correct order.
+        //    var resultcount = result.Count;
+        //    for (int i = 0; i < resultcount; i++)
+        //    {
+        //        var resultItem = result[i];
+        //        if (i + 1 > Cards.Count || !Cards[i].Equals(resultItem))
+        //        {
+        //            Cards.Insert(i, resultItem);
+        //        }
+        //    }
+        //}
+
+
+        public void LoadCards()
+        {
+            List<Card> tempCards = (from p in DbConnection.Table<Card>() select p).ToList();
+
+            foreach(var card in tempCards)
+            {
+                _AllCards.Add(card);
+                Cards.Add(card);
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LoadCards)));
+
+        }
+
+        public void PopulateDatabase()
+        {
+            
+            foreach (var card in _AllCards)
+            {
+                try
+                {
+                    foreach (var color in card.colors)
+                    {
+                        card.colorsBlobbed += (color + " ");
+                    }
+                    foreach (var colorIdentity in card.colorIdentity)
+                    {
+                        card.colorIdentityBlobbed += (colorIdentity + " ");
+                    }
+                    foreach (var type in card.types)
+                    {
+                        card.typesBlobbed += (type + " ");
+                    }
+                    foreach (var subtype in card.subtypes)
+                    {
+                        card.subtypesBlobbed += (subtype + " ");
+                    }
+                    foreach (var printing in card.printings)
+                    {
+                        card.printingsBlobbed += (card.printings + " ");
+                    }
+                    foreach (var legality in card.legalities)
+                    {
+                        card.legalitiesBlobbed += (card.legalities + " ");
+                    }
+                    foreach (var ruling in card.rulings)
+                    {
+                        card.rulingsBlobbed += (card.rulings + " ");
+                    }
+                    foreach (var supertype in card.supertypes)
+                    {
+                        card.supertypesBlobbed += (card.supertypes + " ");
+                    }
+                }
+                catch { }
+                
+              
+                DbConnection.InsertOrReplace(card);
+            }
+
+            LoadCards(); 
+        }
+
+        public async void CallApi()
+        {
+            _AllCards = new List<Card>();
+            HttpClient webClient = new HttpClient();
+
+            var http = new HttpClient();
+            var response = await http.GetAsync(gatherer);
+            APIResult = await response.Content.ReadAsStringAsync();
+
+            var temp = JsonConvert.DeserializeObject<RootObject>(APIResult);
+
+            foreach (var card in temp.cards)
+            {
+                _AllCards.Add(card);
+            }
+
+            PopulateDatabase();
+
+        }
+
+        public SQLiteConnection DbConnection
+        {
+            get
+            {
+                return new SQLiteConnection(
+                    new SQLitePlatformWinRT(),
+                    Path.Combine(ApplicationData.Current.LocalFolder.Path, "Storage.sqlite"));
+            }
+        }
+    }
+}
